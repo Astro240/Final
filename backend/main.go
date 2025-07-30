5e650d473943ca5e7ca8a8a2e47649139804df5b
Astro240 <Abdullaj240@gmail.com>
2025-07-31 02:55:55 +0300

    adding some apis for the website

--- backend/main.go
+++ backend/main.go
@@ -6,15 +6,14 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		//serve the index.html file
 		http.ServeFile(w, r, "../frontend/index.html")
 	})
-	//handle all the css and js files in the src
+	//handle the src, img, and data directories
 	http.Handle("/src/", http.StripPrefix("/src/", http.FileServer(http.Dir("../frontend/src"))))
-	//handle all the images in the img folder
 	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("../frontend/img"))))
-	//serve the json file for the frontend
 	http.Handle("/data/", http.StripPrefix("/data/", http.FileServer(http.Dir("../frontend/data"))))
 
-	http.ListenAndServe("0.0.0.0:8080", nil)
+	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
+		panic(err)
+	}
 }
